37e42f41f3697e19f1ce8fb692621edf91f84118
Suvansh <suvanshd27@gmail.com>
2024-05-18 23:16:50 +0530

    If-else and boolean statements

--- 3. Build a Blackjack game/06. The if...else statement for our game/index.js
+++ 3. Build a Blackjack game/06. The if...else statement for our game/index.js
@@ -1,6 +1,6 @@
-let firstCard = 10
-let secondCard = 7
-let sum = firstCard + secondCard
+let firstCard = 10;
+let secondCard = 7;
+let sum = firstCard + secondCard;
 
 // Write the conditional according to these rules:
 
@@ -8,3 +8,10 @@ let sum = firstCard + secondCard
 // else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
 // else -> "You're out of the game! 😭"
 
+if (sum <= 20) {
+  console.log("Do you want to draw a new card? 🙂");
+} else if (sum === 21) {
+  console.log("Wohoo! You've got Blackjack! 🥳");
+} else {
+  console.log("You're out of the game! 😭");
+}
--- 3. Build a Blackjack game/08. Add the isAlive variable/index.js
+++ 3. Build a Blackjack game/08. Add the isAlive variable/index.js
@@ -1,17 +1,20 @@
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
-let hasBlackJack = false
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
 // 1. Create a variable called isAlive and assign it to true
+let isAlive = true;
 
-// 2. Flip its value to false in the appropriate code block 
+// 2. Flip its value to false in the appropriate code block
 if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
+  console.log("Do you want to draw a new card? 🙂");
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-    hasBlackJack = true
+  console.log("Wohoo! You've got Blackjack! 🥳");
+  hasBlackJack = true;
 } else {
-    console.log("You're out of the game! 😭")
+  console.log("You're out of the game! 😭");
+  isAlive = false;
 }
 
 // 3. Log it out to check that you're doing it right
+console.log(isAlive);
