9e8e717a5dfd1ad58195222ffd44011167f11aec
Suvansh <suvanshd27@gmail.com>
2024-05-20 20:58:43 +0530

    added new card button

--- 3. Build a Blackjack game/14. Display the message/index.js
+++ 3. Build a Blackjack game/14. Display the message/index.js
@@ -10,12 +10,12 @@ let messageEl = document.getElementById("message-el");
 
 function startGame() {
   if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
+    message = "Do you want to draw a new card? ";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
+    message = "Wohoo! You've got Blackjack! ";
     hasBlackJack = true;
   } else {
-    message = "You're out of the game! 😭";
+    message = "You're out of the game! ";
     isAlive = false;
   }
   // 2. Display the message in the messageEl using messageEl.textContent
