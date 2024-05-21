d417fb42fab2f3f8aa14bcdf0ff1f7ba1d709265
Suvansh <suvanshd27@gmail.com>
2024-05-22 00:31:46 +0530

    Completed up the blackJack game

--- 3. Build a Blackjack game/46. Write your first logical operator/index.js
+++ 3. Build a Blackjack game/46. Write your first logical operator/index.js
@@ -1,11 +1,13 @@
-let hasSolvedChallenge = false
-let hasHintsLeft = false
+let hasSolvedChallenge = false;
+let hasHintsLeft = false;
 
 // Create an if statement that checks that both variables are false.
 // If so, run the showSolution() function
 
-function showSolution() {
-    console.log("Showing the solution....")
+if (hasSolvedChallenge === false && hasHintsLeft === false) {
+  showSolution();
 }
 
-
+function showSolution() {
+  console.log("Showing the solution....");
+}
--- 3. Build a Blackjack game/51. Create your first object/index.js
+++ 3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,4 +2,12 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
+let airbnbCastle = {
+  isAvailable: true,
+  location: "Bali",
+  price: 100,
+  amenities: ["pool", "wifi", "breakfast"],
+};
 
+console.log(airbnbCastle.location);
+console.log(airbnbCastle.amenities);
